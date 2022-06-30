7e21ecf3beb79126645db763b53a56719e1ecd30
LauM23 <laura.marinelli.01@gmail.com>
2022-06-30 16:49:47 +0200

    aaa

--- AbissiCurallo/src/abissicurallo/FinestraIniziale.java
+++ AbissiCurallo/src/abissicurallo/FinestraIniziale.java
@@ -14,6 +14,8 @@ public class FinestraIniziale extends javax.swing.JFrame {
      * Creates new form FinestraIniziale
      */
     public FinestraIniziale() {
+        
+        setUndecorated (true);
         initComponents();
     }
 
@@ -27,16 +29,17 @@ public class FinestraIniziale extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(360, 640));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 360, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 640, Short.MAX_VALUE)
         );
 
         pack();
